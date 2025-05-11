ec0d608f455cfb6828baff5c5063e4683c88c144
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-05-11 14:20:39 -0500

    feat(dashboard): implement dashboard layout and animal cards

--- src/app/rancher/components/dashboard/dashboard.component.ts
+++ src/app/rancher/components/dashboard/dashboard.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  animals = Array(4).fill({
+    nombre: 'Rascas',
+    especie: 'Vaca',
+    edad: '5 años',
+    sexo: 'Macho',
+    enfermedades: 'No',
+    imagen: '/assets/img/vaca.jpg'
+  });
 }
