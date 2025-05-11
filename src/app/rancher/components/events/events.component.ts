8f2e774fed4ee51041b5c64400450e8ff5fa0fb9
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-05-11 17:20:39 -0500

    feat(events): add footer component to events page

--- src/app/rancher/components/events/events.component.ts
+++ src/app/rancher/components/events/events.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FooterComponentComponent } from '../../../public/components/footer-component/footer-component.component';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FooterComponentComponent],
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
